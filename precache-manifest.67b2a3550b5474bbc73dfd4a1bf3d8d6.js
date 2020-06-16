c346aad763a4bd9106589815e8d9f68b116b1f07
VipulKod <vipul.kodle@gmail.com>
2020-06-16 20:24:26 +0530

    Updates

---
+++ precache-manifest.67b2a3550b5474bbc73dfd4a1bf3d8d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d31eef7bb9de4b45190ab2eed8523a",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "e6fee1a4825083d949d1",
+    "url": "/MonsterRolodex/static/css/main.601d100e.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/MonsterRolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterRolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6fee1a4825083d949d1",
+    "url": "/MonsterRolodex/static/js/main.bbbe2e39.chunk.js"
+  },
+  {
+    "revision": "f7e4579952e118716b7d",
+    "url": "/MonsterRolodex/static/js/runtime-main.c6cbea1f.js"
+  }
+]);
